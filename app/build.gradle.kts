c89406f725c5cc951c28569ba193046b09b31850
Heddxh <g311571057@gmail.com>
2023-09-13 21:40:03 +0800

    fetch news and update home list(only text)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.android.build.api.dsl.Packaging
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("org.jetbrains.kotlin.plugin.serialization") version "1.8.10" apply true
 }
 
 android {
@@ -18,6 +21,10 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+    }
+
+    packaging {
+        resources.excludes.add("META-INF/*") //TODO: WHY????
     }
 
     buildTypes {
@@ -59,7 +66,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.ui:ui-util:1.5.0")
+    implementation("androidx.compose.ui:ui-util:1.5.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -67,4 +74,12 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    val ktor_version: String by project
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-logging:$ktor_version")
+    implementation ("io.ktor:ktor-client-encoding:$ktor_version")
+    implementation("io.ktor:ktor-client-android:$ktor_version")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 }
--- app/src/main/java/com/heddxh/kupo/MainActivity.kt
+++ app/src/main/java/com/heddxh/kupo/MainActivity.kt
@@ -32,28 +32,41 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.heddxh.kupo.network.NewsService
+import com.heddxh.kupo.network.dto.News
 import com.heddxh.kupo.ui.theme.KupoTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val service = NewsService.create()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val newsList = produceState<List<News>>(
+                initialValue = emptyList(),
+                producer = {
+                    value = service.getNews()
+                }
+            )
             KupoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Home(imageList = imageData)
+                    Home(newsList.value)
                 }
             }
         }
@@ -62,20 +75,21 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Home(imageList: List<Int>, modifier: Modifier = Modifier) {
+fun Home(newsList: List<News> = fakeNewsList, modifier: Modifier = Modifier) {
     var query by remember { mutableStateOf("") }
     var searchActive by remember { mutableStateOf(false) }
-    var extraTopPd by remember { mutableStateOf(8.dp) }
+    var extraTopPd by remember { mutableStateOf(8.dp) } /*TODO: 正确的全屏动画*/
     Scaffold(
         modifier = modifier,
         topBar = {
             Box(
                 modifier = modifier
-                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
                     .padding(top = extraTopPd),
                 contentAlignment = Alignment.Center
             ) {
                 SearchBar(
+                    modifier = Modifier.fillMaxWidth(),
                     query = query,
                     onQueryChange = { query = it },
                     onSearch = {},
@@ -90,44 +104,64 @@ fun Home(imageList: List<Int>, modifier: Modifier = Modifier) {
                     placeholder = { Text("库啵！") },
                     leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
                 ) {
-                    /*TODO*/
+                    /*TODO: 搜索实现*/
                 }
             }
         }
     ) { padding ->
         Column(modifier = Modifier
             .padding(padding)
-            .padding(top = 32.dp)
         ) {
-            Carousel(imageList = imageList)
+            /*TODO: 把 Carousel 作为垂直滚动的子集*/
             Spacer(Modifier.height(8.dp))
-            Cards()
+            Cards(newsList = newsList)
         }
     }
 }
 
 @Composable
-fun Cards(modifier: Modifier = Modifier) {
+private fun Cards(newsList: List<News>, modifier: Modifier = Modifier) {
     LazyColumn(contentPadding = PaddingValues(16.dp), modifier = modifier) {
-        items(imageData) { item ->
-            CardItem(image = item, Modifier.padding(vertical = 8.dp))
+        items(newsList) { news ->
+            CardItem(
+                image = fetchImage(news.homeImagePath),
+                title = news.title,
+                summary = news.summary,
+                Modifier.padding(vertical = 8.dp)
+            )
         }
     }
 }
 
 @Composable
-fun CardItem(@DrawableRes image: Int, modifier: Modifier = Modifier) {
+private fun CardItem(
+    image: Painter,
+    title: String,
+    summary: String,
+    modifier: Modifier = Modifier
+) {
     Card(modifier = modifier) {
         Image(
-            painter = painterResource(image),
+            painter = image,
             contentDescription = null
         )
+        Column(Modifier.padding(16.dp)) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Text(
+                text = summary,
+                style = MaterialTheme.typography.titleSmall
+            )
+        }
     }
 }
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Carousel(@DrawableRes imageList: List<Int>, modifier: Modifier = Modifier) {
+private fun Carousel(@DrawableRes imageList: List<Int>, modifier: Modifier = Modifier) {
     val pagerState = rememberPagerState(
         initialPage = 0,
         initialPageOffsetFraction = 0f
@@ -149,19 +183,11 @@ fun Carousel(@DrawableRes imageList: List<Int>, modifier: Modifier = Modifier) {
     }
 }
 
-@Preview
+@Preview(device = "id:pixel_4_xl", showBackground = true)
 @Composable
 fun HomePreview() {
     KupoTheme {
-        Home(imageList = imageData)
-    }
-}
-
-@Preview
-@Composable
-fun CardsPreview() {
-    KupoTheme {
-        Cards()
+        Home()
     }
 }
 
@@ -171,4 +197,28 @@ private val imageData = listOf(
     R.drawable.screenshot_20230905_173444, R.drawable.screenshot_20230905_173454,
     R.drawable.screenshot_20230905_174304, R.drawable.screenshot_20230905_214857,
     R.drawable.screenshot_20230905_222346, R.drawable.screenshot_20230905_222813
+)
+
+private val fakeSingleNews = News(
+    link = "https://ff.web.sdo.com/web8/index.html#/newstab/newscont/352734",
+    homeImagePath =  "https://fu5.web.sdo.com/10036/202308/16922658666728.jpg",
+    publishDate =  "2023/08/18 10:59:40",
+    sortIndex = 10,
+    summary = "最终幻想14将于9月9日-9月10日参展北京核聚变2023！",
+    title = "9/9-9/10 《最终幻想14》参展北京核聚变2023！"
+)
+
+@Composable
+private fun fetchImage(path: String): Painter {
+    /*TODO: 加载网络图片*/
+    return painterResource(R.drawable.screenshot_20230905_173454)
+}
+
+private val fakeNewsList = listOf<News>(
+    fakeSingleNews,
+    fakeSingleNews,
+    fakeSingleNews,
+    fakeSingleNews,
+    fakeSingleNews,
+    fakeSingleNews
 )
